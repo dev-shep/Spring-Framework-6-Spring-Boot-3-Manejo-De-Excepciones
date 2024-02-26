b98eedcd2eb25d5965ff34dc8bbac52c408d909e
ksolis <limonsito.kls@hotmail.com>
2024-02-26 05:11:14 +0100

    first commit

---
+++ src/main/java/com/karla/errores/springbooterrores/SpringbootErroresApplication.java
@@ -0,0 +1,13 @@
+package com.karla.errores.springbooterrores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootErroresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootErroresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/karla/errores/springbooterrores/SpringbootErroresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karla.errores.springbooterrores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootErroresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
